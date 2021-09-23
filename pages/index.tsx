edee36c8d860e826d91c26d1616d5040a63b3b22
Senghoo Kim <shkdmb@gmail.com>
2021-09-23 19:38:05 +0800

    feat: remove unpublished article from index

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,9 +3,21 @@ import Main from '../layout/Main'
 import {fetchArticle} from '../api/article'
 import '../styles/index.scss'
 import ArticleHead from '../components/ArticleHead'
+import indexStyle from './index.module.scss'
+import mainStyle from '../layout/Main.module.scss'
+import * as React from 'react'
 
 export async function getEdgeProps() {
-    const articles = await fetchArticle()
+
+    const query = {
+        "filter": {
+            "property": "PublishAt",
+            "date": {
+                is_not_empty:true
+            }
+        }
+    }
+    const articles = await fetchArticle(query)
     return {
         props: {
             articles
@@ -19,10 +31,15 @@ export default function Index({articles}) {
         <>
             <Head>
                 <title>HaoIO - Thinking &amp; Writing</title>
+                <style type="text/css">
+                    {(indexStyle as any)._getCss && (indexStyle as any)._getCss()}
+                </style>
             </Head>
             <Main>
                 {
-                    articles.map(e => <ArticleHead key={e.ID} head={e}/>)
+                    articles.map(e => <div className={indexStyle.article}>
+                        <ArticleHead key={e.ID} head={e}/>
+                    </div>)
                 }
 
             </Main>
