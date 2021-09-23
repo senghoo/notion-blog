388d475fad0566296549f992fd514febf646e0e8
Senghoo Kim <shkdmb@gmail.com>
2021-09-23 23:56:50 +0800

    fix: gist display

--- notion/renders/Embed.tsx
+++ notion/renders/Embed.tsx
@@ -4,9 +4,14 @@ import Gist from 'react-gist'
 export default function paragraph({block}:{block: EmbedBlock}){
     const url = block.embed.url
     const parsedUrl = new URL(url)
+    const isWorker = (typeof document) === 'undefined'
     if (url.startsWith('https://gist.github.com/')){
         const paths = parsedUrl.pathname.split('/')
         const id = paths[paths.length-1]
+
+        if (isWorker){
+            return null
+        }
         return <Gist id={id} />
     }
     return null
--- notion/renders/RichText.tsx
+++ notion/renders/RichText.tsx
@@ -1,16 +1,21 @@
 import {RichText} from '@notionhq/client/build/src/api-types'
-import 'katex/dist/katex.min.css';
-import { InlineMath, BlockMath } from 'react-katex'
+import 'katex/dist/katex.min.css'
+import {InlineMath, BlockMath} from 'react-katex'
 
 export default function (props: { text: RichText[] }) {
+    if (props.text.length === 1 && props.text[0].type === 'equation') {
+        return <BlockMath math={props.text[0].plain_text}/>
+    }
     return <>
-        {props.text.map(rt => {
-            switch (rt.type) {
-                case 'text':
-                    return <>{rt.plain_text}</>
-                case 'equation':
-                    return <InlineMath math={rt.plain_text}/>
-            }
-        })}
+        {
+            props.text.map((rt, idx) => {
+                switch (rt.type) {
+                    case 'text':
+                        return <span key={idx}>{rt.plain_text}</span>
+                    case 'equation':
+                        return <InlineMath key={idx} math={rt.plain_text}/>
+                }
+            })
+        }
     </>
 }
