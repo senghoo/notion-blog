fcd8de2c5de924aeef1caed742190f3ec107d438
Senghoo Kim <shkdmb@gmail.com>
2021-09-26 11:10:40 +0800

    debug: client not run

--- layout/Main.tsx
+++ layout/Main.tsx
@@ -8,65 +8,65 @@ import Footer from './Footer'
 import 'antd/dist/antd.css'
 import mainStyle from './Main.module.scss'
 
-export default function Main({children}:{children: React.FC }) {
-        return (
-            <Layout className={mainStyle.baseLayout}>
-                <div className={mainStyle.headerWrapper}>
-                    <div className={mainStyle.header}>
-                        <div>
-                            <ul>
-                                <li className={mainStyle.active}><a href="/">首页</a></li>
-                                <li><a>Golang</a></li>
-                                <li><a>关于</a></li>
-                            </ul>
-                        </div>
-                        <div>
-                            <ul>
-                                <li><a><i className="fab fa-twitter"/></a></li>
-                                <li><a><i className="fab fa-github"/></a></li>
-                                <li><a><i className="fa fa-rss"/></a></li>
-                            </ul>
-                        </div>
+export default function Main({children}) {
+    return (
+        <Layout className={mainStyle.baseLayout}>
+            <div className={mainStyle.headerWrapper}>
+                <div className={mainStyle.header}>
+                    <div>
+                        <ul>
+                            <li className={mainStyle.active}><a href="/">首页</a></li>
+                            <li><a>Golang</a></li>
+                            <li><a>关于</a></li>
+                        </ul>
+                    </div>
+                    <div>
+                        <ul>
+                            <li><a><i className="fab fa-twitter"/></a></li>
+                            <li><a><i className="fab fa-github"/></a></li>
+                            <li><a><i className="fa fa-rss"/></a></li>
+                        </ul>
                     </div>
                 </div>
-                <div className={mainStyle.mainWrapper}>
-                    <Layout className={mainStyle.main}>
-                        <Content className={mainStyle.contentWrapper}>
-                            <div className={mainStyle.logoWrapper}>
-                                <a className={mainStyle.logo} title="HaoIO"
-                                   href="/" rel="home">
-                                    <img
-                                        src="/HaoIO.png"
-                                        srcSet={`/HaoIO.png 1x, /HaoIO@2x.png 2x`}
-                                        className="site_logo"
-                                        alt="HaoIO"/>
-                                </a>
-                            </div>
-                            <div>
-                                {children}
-                            </div>
-                        </Content>
-                        <Sider
-                            className={mainStyle.side}
-                            width={300}
-                            breakpoint="xl"
-                            collapsedWidth={0}
-                        >
-                            <div className={mainStyle.about}>
-                                <h4 className={mainStyle.sideElem}>
-                                    关于我
-                                </h4>
-                                <img className={mainStyle.sideElem} src="/avatar.png"/>
-                                <p className={mainStyle.sideElem}>
-                                    嗨，你好。 欢迎你到我的小站。 我叫Senghoo，是一名码农，目前攻读博士学位。工作之余喜欢折腾各种新鲜玩意。 联系请发送邮件到me@senghoo.com
-                                </p>
-                            </div>
-                        </Sider>
-                    </Layout>
-                </div>
-                <div className={mainStyle.footerWrap}>
-                    <Footer/>
-                </div>
-            </Layout>
-        )
-    }
+            </div>
+            <div className={mainStyle.mainWrapper}>
+                <Layout className={mainStyle.main}>
+                    <Content className={mainStyle.contentWrapper}>
+                        <div className={mainStyle.logoWrapper}>
+                            <a className={mainStyle.logo} title="HaoIO"
+                               href="/" rel="home">
+                                <img
+                                    src="/HaoIO.png"
+                                    srcSet={`/HaoIO.png 1x, /HaoIO@2x.png 2x`}
+                                    className="site_logo"
+                                    alt="HaoIO"/>
+                            </a>
+                        </div>
+                        <div>
+                            {children}
+                        </div>
+                    </Content>
+                    <Sider
+                        className={mainStyle.side}
+                        width={300}
+                        breakpoint="xl"
+                        collapsedWidth={0}
+                    >
+                        <div className={mainStyle.about}>
+                            <h4 className={mainStyle.sideElem}>
+                                关于我
+                            </h4>
+                            <img className={mainStyle.sideElem} src="/avatar.png"/>
+                            <p className={mainStyle.sideElem}>
+                                嗨，你好。 欢迎你到我的小站。 我叫Senghoo，是一名码农，目前攻读博士学位。工作之余喜欢折腾各种新鲜玩意。 联系请发送邮件到me@senghoo.com
+                            </p>
+                        </div>
+                    </Sider>
+                </Layout>
+            </div>
+            <div className={mainStyle.footerWrap}>
+                <Footer/>
+            </div>
+        </Layout>
+    )
+}
