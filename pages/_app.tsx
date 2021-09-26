9769a39501d85d644a68614075360998c77fc00e
Senghoo Kim <shkdmb@gmail.com>
2021-09-26 10:59:23 +0800

    debug: remove optimizer for client

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,8 @@
 import '@fortawesome/fontawesome-free/js/all.js'
 import '../styles/index.scss'
-import 'rc-dropdown/assets/index.css'
 import 'katex/dist/katex.min.css'
 export default function App({Component, pageProps}) {
+    console.log('App Started')
     return (
         <Component {...pageProps} />
     )
